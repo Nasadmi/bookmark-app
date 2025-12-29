a8c22e103166a0394342d33b03dac5961c286dea
Nasadmi <samueldiego828@gmail.com>
2025-12-29 13:19:18 +0100

    Finish user controller

--- api/src/routes/user.router.js
+++ api/src/routes/user.router.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { validationMiddleware } from "../middleware/validationMiddleware.js";
-import { UserSchema } from "../schemas/user.schema.js";
+import { UpdateUserSchema, UserSchema } from "../schemas/user.schema.js";
 import { User } from "../lib/models.js";
-import { UniqueConstraintError } from "sequelize";
+import { UniqueConstraintError, Error } from "sequelize";
 import { hash } from "bcrypt";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
@@ -16,13 +16,13 @@ router.post("/user", validationMiddleware(UserSchema), async (req, res) => {
       parseInt(process.env.HASH_ROUNDS || "8")
     );
     await User.create({ email, password: hashedPassword });
-    return res.status(200).json({
-      message: "User has been created",
+    return res.status(201).json({
+      message: "Created",
     });
   } catch (err) {
     if (err instanceof UniqueConstraintError) {
       return res.status(409).json({
-        message: "The user already exists",
+        message: "Conflict",
       });
     }
 
@@ -40,7 +40,7 @@ router.get("/user", authMiddleware, async (req, res) => {
 
     if (!user) {
       return res.status(404).json({
-        message: "User doesnt exist",
+        message: "Not Found",
       });
     } else {
       const { password, updatedAt, ...result } = user.dataValues;
@@ -53,5 +53,60 @@ router.get("/user", authMiddleware, async (req, res) => {
     });
   }
 });
+
+router.put(
+  "/user",
+  authMiddleware,
+  validationMiddleware(UpdateUserSchema),
+  async (req, res) => {
+    try {
+      const id = req["user"];
+      if (req.body.password) {
+        req.body.password = await hash(req.body.password, parseInt(process.env.HASH_ROUNDS || '8'))
+      }
+      const newUser = req.body;
+      const [rowsUpdated] = await User.update(newUser, { where: { id } });
+      if (rowsUpdated === 0) {
+        return res.status(404).json({
+          message: "Not Found",
+        });
+      }
+      return res.status(200).json({
+        message: "Updated",
+      });
+    } catch (err) {
+      if (err instanceof UniqueConstraintError) {
+        return res.status(409).json({
+          message: "Conflict",
+        });
+      }
+
+      console.error(err);
+      return res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  }
+);
+
+router.delete('/user', authMiddleware, async (req, res) => {
+  try {
+    const id = req['user']
+    const rowDeleted = await User.destroy({ where: { id } })
+    if (rowDeleted === 0) {
+      return res.status(404).json({
+        message: 'Not Found'
+      })
+    }
+    return res.status(200).json({
+      message: 'Deleted'
+    })
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({
+      message: 'Internal Server Error'
+    })
+  }
+})
 
 export { router as userRoutes };
